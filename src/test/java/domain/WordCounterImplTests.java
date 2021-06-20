add601ecbf6634007056b056feab3a42da001f64
Olesya Esenkova <oesenkova96@mail.ru>
2021-06-20 16:32:53 +0300

    Fixed Tests

--- src/test/java/domain/WordCounterImplTests.java
+++ src/test/java/domain/WordCounterImplTests.java
@@ -1,5 +1,6 @@
 package domain;
 
+import org.apache.logging.log4j.core.Logger;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -13,9 +14,10 @@ public class WordCounterImplTests {
     @Test
     public void getWordsTest() throws Exception{
         WebPageClient webPageClientMock = Mockito.mock(WebPageClient.class);
+        Logger logger = Mockito.mock(Logger.class);
         Mockito.when(webPageClientMock.getText(anyString())).thenReturn("В качестве результата работы пользователь " +
                 "должен получить статистику по количеству уникальных слов в тексте.");
-        WordCounter wordCounter = new WordCounterImpl(webPageClientMock);
+        WordCounter wordCounter = new WordCounterImpl(webPageClientMock, logger);
 
         HashMap<String, Integer> statistics = wordCounter.getWords("https://www.simbirsoft.com/");
 
